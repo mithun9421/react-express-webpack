a6020edf686d0991ad81d528cfbc16f318acd10d
mithun9421 <mithunmuralee94@gmail.com>
2020-11-06 00:06:28 +0530

    Prettifier addition

--- client/app.js
+++ client/app.js
@@ -1,5 +1,5 @@
-import React from 'react';    
-import ReactDOM from 'react-dom';    
-import Main from './components/main';
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./containers/main";
 
-ReactDOM.render(<Main/>, document.getElementById('app'));   
+ReactDOM.render(<Main />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,45 @@
-var webpack=require('webpack');  
-var path=require("path");    
-const SRC_PATH=path.resolve(__dirname,"client");    
-var DIST_PATH=path.resolve(__dirname,"public");   
-var config = {  
-    devtool:'source-map',  
-    entry:[  
-        SRC_PATH+"/app.js"  
-    ],  
-    output:{    
-        path: DIST_PATH,    
-        publicPath: '/',  
-        filename:"bundle.js"    
-    },    
-    resolve: {   
-        extensions: [ '.js', '.jsx']    
-    },  
-   module: {  
-       rules: [  
-                {    
-                    test:/\.js?$/,    
-                    exclude: /node_modules/,    
-                    include: /client/,    
-                    loader:"babel-loader",
-                    options : {
-                        presets: ['es2015'] 
-                    }
-                },  
-                {    
-                    test:/\.jsx?$/,    
-                    exclude: /node_modules/,    
-                    include: /client/,    
-                    loader:"babel-loader",    
-                    options : {
-                        presets: ['es2015','stage-2','react']    
-                    }
-                }  
-            ]  
-        },  
-        devServer:{  
-            hot:true,  
-            port:4500  
-        }  
-}  
-  
-module.exports = config;  
+var webpack = require("webpack");
+var path = require("path");
+const SRC_PATH = path.resolve(__dirname, "client");
+var DIST_PATH = path.resolve(__dirname, "public");
+var config = {
+  devtool: "source-map",
+  entry: [SRC_PATH + "/app.js"],
+  output: {
+    path: DIST_PATH,
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        include: /client/,
+        loader: "babel-loader",
+        options: {
+          presets: ["es2015"],
+        },
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        include: /client/,
+        loader: "babel-loader",
+        options: {
+          presets: ["es2015", "stage-2", "react"],
+        },
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+    port: 4500,
+    publicPath: "/",
+  },
+};
+
+module.exports = config;
